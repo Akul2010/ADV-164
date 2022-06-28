449219e94dd10bc6950eb9e4bb3fa5e656b028bb
Akul2010 <75178259+Akul2010@users.noreply.github.com>
2022-06-28 18:20:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
-from tkinter import *
+rom tkinter import *
+from PIL import ImageTk, Image
 from tkinter import filedialog
-from PIL import ImageTk,Image
 
-root=Tk()
-root.title("Planet Encyclopedia")
-root.geometry("600x600")
-root.configure(background="black")
 
-img_path=""
-def Open():
+root = Tk()
+root.title("Image Viewer")
+root.geometry("500x500")
+root.configure(bg="black")
+
+label_image = Label(root,bg="black",highlightthickness=5)
+label_image.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+img_path = ""
+
+def OpenImage():
     global img_path
-    img_path=filedialog.askopenfilename(title = "Select Image File", filetypes= [("Image Files", "*.jpg *.gif *.png *.jpeg")])
+    img_path = filedialog.askopenfilename(title="Open Image File", filetypes=[("Image Files", "*.jpg *.gif *.png *.jpeg"),])
     print(img_path)
-    img=ImageTk.PhotoImage(Image.open(img_path))
-    label_image.configure(image=img)
-    label_image.image=img
-def rotate():
-    print("Rotate")
-    print(img_path)
-    im=Image.open(img_path)
-    rotated_img=im.rotate(180)
-    img=ImageTk.PhotoImage(rotated_img)
-    label_image.configure(image=img)
-    label_image.image=img
+    im = Image.open(img_path)
+    img = ImageTk.PhotoImage(im)
+    label_image["image"] = img
+    img.close()
     
-btn_open=Button(root,text="Open Image",command=Open,relief="flat",font=("Times New Roman0",15),bg="#808080",fg="white")
-btn_open.place(relx=0.5,rely=0.08,anchor=CENTER)
+btn = Button(root,text="Open Image", bg="gray60", fg="gray97", command=OpenImage)
+btn.place(relx=0.5,rely=0.25,anchor=CENTER)
 
-btn_rotate=Button(root,text="Rotate Image",command=rotate,relief="flat",font=("Times New Roman0",15),bg="#808080",fg="white")
-btn_rotate.place(relx=0.5,rely=0.8,anchor=CENTER)
-
-label_image=Label(root,text="royce",bg="black",fg="black")
-label_image.pack()
 root.mainloop()
